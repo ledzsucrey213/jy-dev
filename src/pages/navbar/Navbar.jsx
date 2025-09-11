c73d96ae34ab1cf09f29b1220e1e253374478da3
Yanis <yanis@MacBook-Pro-de-Yanis.local>
2025-09-11 18:47:27 +0200

    frontend

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       services: "Services",
       how: "Comment ça marche",
       about: "À propos",
-      contact: "LOOOOl",
+      contact: "Contact",
     },
     en: {
       services: "Services",
